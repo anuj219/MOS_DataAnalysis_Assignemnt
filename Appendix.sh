3a207eb41f8ec74a507822b677df5cb644599065
pynuz <152525579+anuj219@users.noreply.github.com>
2025-08-08 16:02:51 +0530

    Update Appendix.sh

--- Appendix.sh
+++ Appendix.sh
@@ -1,3 +1,6 @@
+sed 's/ HTTP\/1.1"//' access_uncleaned.log > access.log
+
+
 wc -l access.log
 awk '{print $7}' access.log | sort | uniq -c | sort -nr | head -n 10
 awk '{print $1}' access.log | sort | uniq -c | sort -nr | head -n 10
@@ -8,3 +11,12 @@ awk '($9 ~ /^4/) {print $9}' access.log | sort | uniq -c | sort -nr
 awk '{if($9==200) count++} END {print "Successful (200): " count}' access.log
 awk '{if($9==404) count++} END {print "Not Found (404): " count}' access.log
 sort -k10 -nr access.log | head -n 5
+
+# Saving an output to a .dat file for visualization
+awk '...' access.log > data_file.dat
+
+# Making gnuplot files
+gedit file_name.gp
+
+# runnung gnuplot
+gnuplot file_name.gp
