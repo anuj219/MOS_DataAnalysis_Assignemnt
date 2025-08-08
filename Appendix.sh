b407b6329ab32d3e680e0ea76af7618a60bf2f6d
pynuz <152525579+anuj219@users.noreply.github.com>
2025-08-09 00:49:02 +0530

    Update Appendix.sh

--- Appendix.sh
+++ Appendix.sh
@@ -11,6 +11,8 @@ awk '($9 ~ /^4/) {print $9}' access.log | sort | uniq -c | sort -nr
 awk '{if($9==200) count++} END {print "Successful (200): " count}' access.log
 awk '{if($9==404) count++} END {print "Not Found (404): " count}' access.log
 sort -k10 -nr access.log | head -n 5
+sed 's/.*\[..\/...\/....:\(..\):..:.. .*/\1/' access.log | sort | uniq -c | sort -n
+awk '($9 ~ /^5/) {print $9}' access.log | sort | uniq -c | sort -nr
 
 # Saving an output to a .dat file for visualization
 awk '...' access.log > data_file.dat
